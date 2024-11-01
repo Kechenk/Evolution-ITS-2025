6e788952482ffd895f34311d651ca2bec6670a16
kechenk <rezhakechenk67@gmail.com>
2024-11-01 21:12:35 +0700

    pengetesan menggunakan koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,19 @@
+˝<?php
+$host = "127.0.0.1";
+$port = "3306";
+$dbname = "123";
+$user = "123";
+$password = "123";
+
+// Create connection
+$conn = new mysqli($host, $user, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+?>
